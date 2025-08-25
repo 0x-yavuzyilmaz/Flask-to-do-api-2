8343887d81a9d678c6ff257032929cb762e531a4
Yavuz Yılmaz <yavuzyilmaz@Yavuzs-MacBook-Pro.local>
2025-08-25 03:46:11 +0300

    feat: Add initial dataset and GET /api/todos endpoint

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+# sahte veritabanı
+todos = [
+    {"id": 1, "task": "Build a REST API", "done": True},
+    {"id": 2, "task": "Create a GET endpoint", "done": False}
+]
+
+
+@app.route('/api/todos')
+def get_all_todos():
+    return jsonify(todos)
+
 
 @app.route('/')
 def home():
     return "Todo API server is running"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
